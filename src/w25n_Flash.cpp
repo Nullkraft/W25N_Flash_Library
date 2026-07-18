eab30b018b44287bf88c39ae6df7dbfd474ef990
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2026-07-18 11:42:01 -0400

    Remove main and change to 'class W25N_Flash'

---
+++ src/w25n_Flash.cpp
@@ -0,0 +1,5 @@
+#include "w25n_Flash.h"
+
+uint32_t W25N_Flash::readManufID(uint8_t ID) {
+  // do nothing
+}
---
+++ src/w25n_Flash.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+class W25N_Flash {
+  private:
+    W25N_Flash();
+    static uint32_t manufID;
+  public:
+    uint8_t jedecIDcmd = 0x9F;
+    uint32_t readManufID(uint8_t ID);
+};
