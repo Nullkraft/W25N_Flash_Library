9513bb4ffc9e0f36269e50de6b74ce8252f2b5a7
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2026-07-26 15:07:04 -0400

    Create constructor

--- src/w25n_Flash.cpp
+++ src/w25n_Flash.cpp
@@ -1,10 +1,15 @@
 #include "w25n_Flash.h"
 
+W25N_Flash::W25N_Flash() {
+  // ctor
+}
+
 uint32_t W25N_Flash::readManufID(uint8_t ID) {
   // do nothing
   return 0;
 }
 
 uint32_t W25N_Flash::getJedecID() {
-  return 0xEFAA21;
+  return 0xAA21;
 }
+
--- src/w25n_Flash.h
+++ src/w25n_Flash.h
@@ -2,10 +2,13 @@
 
 class W25N_Flash {
   private:
-    W25N_Flash();
-    static uint32_t manufID;
+    const static uint32_t manufID = 0xAA21;
+
   public:
+    W25N_Flash();
     uint8_t jedecIDcmd = 0x9F;
     uint32_t readManufID(uint8_t ID);
     uint32_t getJedecID();
+
+    void spiWriteRegister(uint8_t sel_pin, uint32_t value);
 };
